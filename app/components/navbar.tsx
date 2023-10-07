8abc181c39df0c09ed0975ed4da7e976ba5fa476
mind0bender <mind0bender@gmail.com>
2023-10-07 10:29:05 +0530

    { "CREATE": ["Layout", "navbar"] }

---
+++ app/components/navbar.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import React from "react";
+
+interface NavbarProps {
+  title?: string;
+  links?: NavbarLink[];
+}
+
+interface NavbarLink {
+  name: string;
+  url: string;
+}
+
+const navbarLinks: NavbarLink[] = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Services",
+    url: "/services",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+  {
+    name: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
+];
+
+const Navbar: React.FC<NavbarProps> = ({
+  title = "Echo Elevate",
+  links = navbarLinks,
+}) => {
+  return (
+    <nav
+      className={`flex flex-col bg-slate-100 px-8 py-6 gap-4 border-b border-b-slate-400`}
+    >
+      <h1 className={`text-2xl font-semibold`}>{title}</h1>
+      <hr className={`border-slate-950`} />
+      <ul className={`flex justify-around items-center gap-4 text-xl`}>
+        {links.map((link) => (
+          <li key={link.url}>
+            <Link
+              className={`text-slate-700 hover:text-slate-950`}
+              href={link.url}
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
